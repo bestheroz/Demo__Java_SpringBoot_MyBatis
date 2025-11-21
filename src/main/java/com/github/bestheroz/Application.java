bc8d10a491dcac00add7c46b0f980ee2f02cf1bf
joony.kim <bestheroz@gmail.com>
2025-11-21 21:41:46 +0900

    💥jdk25 + spring boot 4

--- src/main/java/com/github/bestheroz/Application.java
+++ src/main/java/com/github/bestheroz/Application.java
@@ -2,11 +2,19 @@ package com.github.bestheroz;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.data.jpa.autoconfigure.DataJpaRepositoriesAutoConfiguration;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
 
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+      HibernateJpaAutoConfiguration.class,
+      DataJpaRepositoriesAutoConfiguration.class,
+      MybatisAutoConfiguration.class
+    })
 public class Application {
 
   public static void main(final String[] args) {
--- src/main/java/com/github/bestheroz/standard/config/MyBatisConfig.java
+++ src/main/java/com/github/bestheroz/standard/config/MyBatisConfig.java
@@ -3,14 +3,43 @@ package com.github.bestheroz.standard.config;
 import com.github.bestheroz.standard.common.enums.AuthorityEnum;
 import com.github.bestheroz.standard.common.mybatis.handler.GenericListTypeHandler;
 import java.util.List;
+import javax.sql.DataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @Configuration
 @MapperScan("com.github.bestheroz")
 public class MyBatisConfig {
+  @Bean
+  public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+    SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+    sessionFactory.setDataSource(dataSource);
+
+    // Try to load mapper XML files if they exist
+    try {
+      sessionFactory.setMapperLocations(
+          new PathMatchingResourcePatternResolver()
+              .getResources("classpath*:mybatis/mapper/**/*.xml"));
+    } catch (Exception e) {
+      // Mapper XMLs are optional, can use annotations instead
+    }
+
+    org.apache.ibatis.session.Configuration configuration =
+        new org.apache.ibatis.session.Configuration();
+    configuration.setMapUnderscoreToCamelCase(true);
+    configuration
+        .getTypeHandlerRegistry()
+        .register(List.class, new GenericListTypeHandler<>(AuthorityEnum.class));
+
+    sessionFactory.setConfiguration(configuration);
+    return sessionFactory.getObject();
+  }
+
   @Bean
   public ConfigurationCustomizer mybatisConfigurationCustomizer() {
     return configuration ->
